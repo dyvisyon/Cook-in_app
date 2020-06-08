ac3de3c59a353c93e7744a0e5cd2f8d600c70589
Kevin <r_kevin@hotmail.fr>
2020-06-08 11:59:36 +0200

    Json Started

--- Cook'in/MesRecettes.swift
+++ Cook'in/MesRecettes.swift
@@ -1,16 +1,85 @@
 //
 //  MesRecettes.swift
-//  
+//
 //
 //  Created by Ivann Galic on 04/06/2020.
 //
 
 import SwiftUI
 
-struct MesRecettes: View {
-    var body: some View {
-        Text("Mes recettes")
+struct Model: Codable, Identifiable {
+    
+    enum CodingKeys: CodingKey {
+        case title
+        case category
+        case ingredients
+        case steps
     }
+    
+    var id = UUID()
+    var title, category: String
+    var ingredients: [String]
+    var steps: [String]
+}
+
+class Json: ObservableObject {
+    @Published var json = [Model]()
+    
+    init() {
+        load()
+    }
+    
+    func load() {
+        let path = Bundle.main.path(forResource: "Recipes", ofType: "json")
+        let url = URL(fileURLWithPath: path!)
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            do {
+                if let data = data {
+                    let json = try JSONDecoder().decode([Model].self, from: data)
+                    
+                    DispatchQueue.main.sync {
+                        self.json = json
+                    }
+                } else {
+                    print("No data")
+                }
+            } catch {
+                print(error)
+            }
+        }.resume()
+    }
+}
+
+
+    
+    
+    
+struct MesRecettes: View {
+    
+    @ObservedObject var datas = Json()
+    
+    var body: some View {
+        NavigationView {
+            List(datas.json) { item in
+                Text(item.title)
+                Text(item.category)
+                ForEach(0..<item.ingredients.count) { x in
+                    Text(item.ingredients[x])
+                }
+                ForEach(0..<item.steps.count) { x in
+                    Text(item.steps[x])
+                }
+                
+                
+                    
+                
+            }
+        .navigationBarTitle("Mes Recettes")
+        .navigationBarItems(trailing: EditButton())
+        }
+    }
+    
 }
 
 struct MesRecettes_Previews: PreviewProvider {
@@ -18,3 +87,4 @@ struct MesRecettes_Previews: PreviewProvider {
         MesRecettes()
     }
 }
+
